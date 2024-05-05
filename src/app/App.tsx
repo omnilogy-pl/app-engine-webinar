e123a745704baac6925c72776e22436baff6a532
Veranika Kasparevych <veranikabarel@gmail.com>
2024-05-05 22:06:00 +0200

    improve visualisation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,15 +5,17 @@ import {
   convertQueryResultToTimeseries,
   Heading,
   Page,
+  ProgressCircle,
   Surface,
   TimeseriesChart,
 } from "@dynatrace/strato-components-preview";
+import { Colors } from "@dynatrace/strato-design-tokens";
 import React from "react";
 
 export const App = () => {
   const query = `fetch bizevents, from: now()-7d, to: now() | filter source == "CurrencyLayer" and type =="currency.rates" | parse data, "JSON:newData" | fieldsAdd time = timestampFromUnixSeconds(newData[timestamp]) | summarize total=count(), by: { date=bin(toTimestamp(time), 12h), currency=newData[currency], currencyRate=newData[rate] } | fields timeframe=timeframe(from:date, to:date + 1min), currency=currency, currencyRate=currencyRate`;
 
-  const { data } = useDqlQuery({
+  const { data, isLoading } = useDqlQuery({
     body: { query },
   });
   return (
@@ -26,7 +28,23 @@ export const App = () => {
       <Page.Main>
         <Surface minHeight={400}>
           <Heading>Currency rates</Heading>
-          {data && <TimeseriesChart data={convertQueryResultToTimeseries(data)}></TimeseriesChart>}
+          {isLoading && <ProgressCircle />}
+          {data && (
+            <TimeseriesChart
+              data={convertQueryResultToTimeseries(data)}
+              variant="line"
+              gapPolicy="connect"
+              colorPalette="purple-rain"
+              curve="smooth"
+            >
+              <TimeseriesChart.YAxis formatter={(value) => `${value} PLN`} />
+              <TimeseriesChart.Threshold
+                data={{ value: 0.3 }}
+                color={Colors.Charts.Threshold.Bad.Default}
+                label="Some threshold label"
+              />
+            </TimeseriesChart>
+          )}
         </Surface>
       </Page.Main>
     </Page>
